33720ca25ac5ffb634ed50fad9c8ad5fa5badcba
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2021-03-28 00:38:43 -0400

    gasdsad

--- apps/straw_hat_idp/lib/straw_hat_idp/event_store/type_provider.ex
+++ apps/straw_hat_idp/lib/straw_hat_idp/event_store/type_provider.ex
@@ -1,11 +1,11 @@
 defmodule StrawHat.Idp.EventStore.TypeProvider do
   @behaviour Commanded.EventStore.TypeProvider
 
-  # def to_struct(event_name) do
-  #   raise ArgumentError,
-  #         "#{inspect(event_name)} event name is not registered. " <>
-  #           "Please visit #{inspect(__MODULE__)} to register new events."
-  # end
+  def to_struct(event_name) do
+    raise ArgumentError,
+          "#{inspect(event_name)} event name is not registered in the event type mapper." <>
+            "Please visit #{inspect(__MODULE__)} to register new events."
+  end
 
   def to_string(event_struct) do
     raise ArgumentError,
